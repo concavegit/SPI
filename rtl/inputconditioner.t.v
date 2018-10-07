97abe52c708b6ff75a05361d7cb63cadfb5c363f
djconnolly27 <djconnolly27@gmail.com>
2018-10-07 19:34:34 -0400

    Updates input conditioner

--- rtl/inputconditioner.t.v
+++ rtl/inputconditioner.t.v
@@ -1,3 +1,4 @@
+`timescale 1 ns / 1 ps
 `include "inputconditioner.v"
 
 module testconditioner();
@@ -11,18 +12,32 @@ module testconditioner();
                         .positiveedge(rising),
                         .negativeedge(falling)
                         );
-   
-   always #10 clk = !clk;
+   // Generate clock (50MHz)
+   initial clk=0;
+   always #10 clk = !clk; // 50MHz Clock
 
+   // Should suppress glitches up to 79 ns
    initial begin
       $dumpfile("inputconditioner.vcd");
       $dumpvars();
       clk = 0;
-      pin <= 1;
+      pin <= 0;
+      #80 pin <= 0;
+      #80 pin <= 1;
+      #80 pin <= 0;
+      #79 pin <= 1;
+      #80 pin <= 0;
+      #20 pin <= 1;
+      #80 pin <= 1;
+      #30 pin <= 1;
       #10 pin <= 0;
-      #10 pin <= 1;
-      #50 pin <= 0;
-      #100 pin <= 0;
-      #900 $finish;
+      #80 pin <= 1;
+      #80 pin <= 0;
+      #20 pin <= 1;
+      #80 pin <= 0;
+      #80 pin <= 1;
+      #30 pin <= 0;
+      #80 pin <= 1;
+      #100 $finish;
    end
 endmodule
--- rtl/inputconditioner.v
+++ rtl/inputconditioner.v
@@ -7,18 +7,26 @@
 
 module inputconditioner
   (
-   input      clk,
-   input      noisysignal,
+   input      clk, // Clock domain to synchronize input to
+   input      noisysignal, // (Potentially) noisy input signal
    output reg conditioned, positiveedge, negativeedge
+   // Conditioned output signal
+   // 1 clk pulse at rising edge of conditioned
+   // 1 clk pulse at falling edge of conditioned
    );
-   parameter counterwidth = 3;
-   parameter waittime = 3;
+   parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
+   parameter waittime = 3; // Debounce delay, in clock cycles
 
    reg [counterwidth - 1:0] counter = 0;
    reg                      synchronizer0 = 0;
    reg                      synchronizer1 = 0;
 
    always @(posedge clk) begin
+     if (conditioned !== 0 && conditioned !== 1) begin
+         conditioned <= 0;
+         positiveedge <= 0;
+         negativeedge <= 0;
+       end
       if (conditioned == synchronizer1) counter <= 0;
       else begin
          if (counter == waittime) begin
@@ -34,4 +42,4 @@ module inputconditioner
       synchronizer0 <= noisysignal;
       synchronizer1 <= synchronizer0;
    end
-endmodule 
+endmodule
