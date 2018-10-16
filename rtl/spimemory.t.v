71f8878363f32432a8b1b1b8602d3aae269bba76
Kawin Nikomborirak <concavemail@gmail.com>
2018-10-16 19:04:28 +0000

    Use blocking assignments

--- rtl/spimemory.t.v
+++ rtl/spimemory.t.v
@@ -19,45 +19,45 @@ module spitest();
       $dumpfile("spimemory.vcd");
       $dumpvars();
 
-      cs_pin <= 1;
-      mosi_pin <= 0;
-      sclk_pin <= 0;
-      #1000 sclk_pin <= 1;
+      cs_pin = 1;
+      mosi_pin = 0;
+      sclk_pin = 0;
+      #1000 sclk_pin = 1;
       #1000 cs_pin = 0;#1000;
 
       // Write 0xFF to address 0x00;
 
       for (i = 0; i < 16; i = i + 1) begin
-         if (i < 8) mosi_pin <= 0;
-         else mosi_pin <= 1;
-         sclk_pin <= 0;
-         #1000 sclk_pin <= 1; #1000;
+         if (i < 8) mosi_pin = 0;
+         else mosi_pin = 1;
+         sclk_pin = 0;
+         #1000 sclk_pin = 1; #1000;
       end
 
       // Lie dormant for a bit
-      cs_pin <= 1;
-      sclk_pin <= 0;
-      #1000 sclk_pin <= 1;
-      #1000 cs_pin <= 0;#1000;
+      cs_pin = 1;
+      sclk_pin = 0;
+      #1000 sclk_pin = 1;
+      #1000 cs_pin = 0;#1000;
 
 
       // Read 0xFF
       for (i = 0; i < 8; i = i + 1) begin
-         if (i < 7 ) mosi_pin <= 0;
-         else mosi_pin<=1;
-         sclk_pin <= 0;
-         #1000 sclk_pin <= 1; #1000;
+         if (i < 7 ) mosi_pin = 0;
+         else mosi_pin = 1;
+         sclk_pin = 0;
+         #1000 sclk_pin = 1; #1000;
       end
 
       // sr lag
-      sclk_pin <= 0;
-      #1000 sclk_pin <= 1; #1000;
+      sclk_pin = 0;
+      #1000 sclk_pin = 1; #1000;
 
       // Check the output of MISO
       $display("Reading data at address 0");
       for (i = 0; i < 8; i = i + 1) begin
-         sclk_pin <= 0;
-         #1000 sclk_pin <= 1; #1000;
+         sclk_pin = 0;
+         #1000 sclk_pin = 1; #1000;
          $display("%b", miso_pin);
       end
       #1000 $finish;
