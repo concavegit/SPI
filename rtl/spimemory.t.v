92b218a3d02aa56e4f10f681bd4ee19f8276e01b
Kawin Nikomborirak <concavemail@gmail.com>
2018-10-19 00:39:30 +0000

    Push working

--- rtl/spimemory.t.v
+++ rtl/spimemory.t.v
@@ -8,25 +8,27 @@ module spitest();
    reg  mosi_pin;
    wire [3:0] leds;
 
-   integer    i;
+   reg [4:0]    i;
+   
 
    initial clk=0;
    always #1 clk= ~clk;
 
 
    spiMemory dut(clk, sclk_pin, cs_pin, miso_pin, mosi_pin, leds[3:0]);
+   reg [7:0]  test_case;
    initial begin
       $dumpfile("spimemory.vcd");
       $dumpvars();
 
+      test_case = 8'b10110001;
       cs_pin = 1;
       mosi_pin = 0;
       sclk_pin = 0;
       #100 sclk_pin = 1;
       #100 cs_pin = 0;#100;
 
-      // Write 0xFF to address 0x00;
-
+      // Write 8'b11001110 to address 0x00;
       for (i = 0; i < 16; i = i + 1) begin
          if (i < 8) mosi_pin = 0;
          else mosi_pin = 1;
@@ -49,12 +51,8 @@ module spitest();
          #100 sclk_pin = 1; #100;
       end
 
-      // Reach 
-      // sclk_pin = 0; #100;
-      // s
-
       // Check the output of MISO
-      $display("Reading data at address 0");
+      $display("Reading data at address 0, should write ");
       for (i = 0; i < 10; i = i + 1) begin
          sclk_pin = 1;
          if (i > 7) cs_pin = 1;
@@ -67,11 +65,5 @@ module spitest();
 
       // Write 010101 to address 0x11001100110
 
-      for (i = 0; i < 16; i = i + 1) begin
-         if (i < 8) mosi_pin = 0;
-         else mosi_pin = 1;
-         sclk_pin = 0;
-         #100 sclk_pin = 1; #100;
-      end
    end
 endmodule
