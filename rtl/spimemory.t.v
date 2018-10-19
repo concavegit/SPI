a58027979eb935615d33f2cce4d651bdb983cbb6
Kawin Nikomborirak <concavemail@gmail.com>
2018-10-19 13:10:40 +0000

    Upload to master

--- rtl/spimemory.t.v
+++ rtl/spimemory.t.v
@@ -11,6 +11,8 @@ module spitest();
    integer    i;
    reg [7:0]        test_case, test_case_result;
    reg [6:0]        test_case_addr;
+   reg [7:0]        test_case_read;
+   reg [7:0]        j;
 
    initial clk=0;
    always #1 clk= ~clk;
@@ -23,17 +25,18 @@ module spitest();
       $dumpvars();
 
       test_case = 8'b10110001;
+      test_case_addr = 7'b1100001;
       cs_pin = 1;
       mosi_pin = 0;
       sclk_pin = 0;
       #100 sclk_pin = 1;
       #100 cs_pin = 0;#100;
 
-      // Write 8'b11001110 to address 7'b1111111
+      // Write 8'10110001 to address 1100001.
       for (i = 0; i < 16; i = i + 1) begin
-         if (i < 7) mosi_pin = 1;
-         else if (i == 7) mosi_pin = 0; // Signal that this is a write operation
-         else mosi_pin = test_case[i - 8]; // Inverts the test case
+         if (i < 7) mosi_pin = test_case_addr[i];
+         else if (i == 7) mosi_pin = 0;
+         else mosi_pin = test_case[15 - i];
 
          sclk_pin = 0;
          #100 sclk_pin = 1; #100;
@@ -42,98 +45,125 @@ module spitest();
       // Lie dormant for a bit
       cs_pin = 1;
       #1000;
-
+      
       sclk_pin = 0;
       #100 sclk_pin = 1;
       #100 cs_pin = 0;#100;
 
 
-      // Read 0xFF
+      // Read from 1100001
       for (i = 0; i < 8; i = i + 1) begin
-         if (i < 7) mosi_pin = 1;
          if (i == 7) mosi_pin = 1;
+         else  mosi_pin = test_case_addr[i];
          sclk_pin = 0;
          #100 sclk_pin = 1; #100;
       end
 
       // Check the output of MISO
-      $display("Reading data for test 1, should write 10001101:");
-      for (i = 0; i < 8; i = i + 1) begin
+      for (i = 0; i < 10; i = i + 1) begin
          sclk_pin = 1;
+         if (i > 7) cs_pin = 1;
          #100 sclk_pin = 0; #100;
          $display("%b", miso_pin);
       end
-      #100;
 
+      // Test Case 2: Write a test case to all addresses
+      test_case = 8'b10110011;
+      for (j = 0; j < 128; j = j + 1) begin
+         cs_pin = 1;
+         sclk_pin = 0;
+         #100 sclk_pin = 1;
+         #100 cs_pin = 0;#100;
+         test_case_addr = j[6:0];
+         for (i = 0; i < 16; i = i + 1) begin
+            if (i < 7) mosi_pin = test_case_addr[i];
+            else if (i == 7) mosi_pin = 0;
+            else mosi_pin = test_case[15 - i];
 
-
-      // Test Case 2
-      cs_pin = 1;
-      mosi_pin = 0;
-      sclk_pin = 0;
-      #2000 sclk_pin = 1;
-      #100 cs_pin = 0;sclk_pin=0;
-
-      //Master Setting Address
-
-      //Writing to Address
-      // Writes to address 7'b1010101
-      for (i = 0; i < 7; i = i + 1) begin
-          if (i%2 == 0) mosi_pin = 1;
-          else mosi_pin = 0;
-          sclk_pin = 0;
-          #100 sclk_pin = 1; #100;
+            sclk_pin = 0;
+            #100 sclk_pin = 1; #100;
+         end
       end
+      $display("If you see any messages below this point, those are errors.");
 
-      //Read or Write Bit
-      // Set to Write Here
-      sclk_pin = 0;
-      mosi_pin = 0;
-      #100; sclk_pin = 1; #100;
-
-
-      //Writing Data
-      //Writing 8'b10010010
-      for (i = 0; i < 8; i = i + 1) begin
-          if (i%3 == 0) mosi_pin = 1;
-          else mosi_pin = 0;
-          sclk_pin = 0;
-          #100 sclk_pin = 1; #100;
-      end
+      // Read all registers, they should all be 1011011
+      for (j = 0; j < 128; j = j + 1) begin
 
       // Lie dormant for a bit
       cs_pin = 1;
+      #1000;
+      
       sclk_pin = 0;
       #100 sclk_pin = 1;
-      #100 cs_pin = 0;sclk_pin=0;
+      #100 cs_pin = 0;#100;
+         test_case_addr = j[6:0];
+         for (i = 0; i < 8; i = i + 1) begin
+            if (i == 7) mosi_pin = 1;
+            else  mosi_pin = test_case_addr[i];
+            sclk_pin = 0;
+            #100 sclk_pin = 1; #100;
+         end
 
-      //Master Reading Address
+         // Check the output of MISO
+         for (i = 0; i < 10; i = i + 1) begin
+            sclk_pin = 1;
+            if (i > 7) cs_pin = 1;
+            #100 sclk_pin = 0; #100;
+            // test_case_read[i] = miso_pin;
+            if (test_case_read != 8'b1011011) $display("Failed to read 1011011 from register %b", test_case_addr);
+            
+            // $display("%b", miso_pin);
 
-      //Read from Address
-      // Reads address 7'b1010101
-      for (i = 0; i < 7; i = i + 1) begin
-          if (i%2 == 0) mosi_pin = 1;
-          else mosi_pin = 0;
-          sclk_pin = 0;
-          #100 sclk_pin = 1; #100;
+         end
       end
 
-      //Read or Write Bit
-      // Set to Read Here
-      sclk_pin = 0;
-      mosi_pin = 1;
-      #100; sclk_pin = 1; #100;
+      // Test Case 3: Write 10000010 to all addresses, but with CS HIGH and checking for tri-state
+      test_case = 8'b10000010;
+      for (j = 0; j < 128; j = j + 1) begin
+         cs_pin = 1;
+         sclk_pin = 0;
+         #100 sclk_pin = 1;
+         #100 cs_pin = 1;#100;
+         test_case_addr = j[6:0];
+         for (i = 0; i < 16; i = i + 1) begin
+            if (i < 7) mosi_pin = test_case_addr[i];
+            else if (i == 7) mosi_pin = 0;
+            else mosi_pin = test_case[15 - i];
 
-      // Check the output of MISO
-      $display("Reading data for test 2, should write 10010010:");
-      for (i = 0; i < 8; i = i + 1) begin
-         sclk_pin = 1;
-         #100 sclk_pin = 0; #100;
-         $display("%b", miso_pin);
+            sclk_pin = 0;
+            #100 sclk_pin = 1; #100;
+            if (miso_pin != 1'bz) $display("Failed to tristate");
+         end
       end
-      #100;
 
+      // Read all registers, they should all still be 1011011
+      for (j = 0; j < 128; j = j + 1) begin
 
-      $finish();
+         // Lie dormant for a bit
+         cs_pin = 1;
+         #1000;
+         
+         sclk_pin = 0;
+         #100 sclk_pin = 1;
+         #100 cs_pin = 0;#100;
+         test_case_addr = j[6:0];
+         for (i = 0; i < 8; i = i + 1) begin
+            if (i == 7) mosi_pin = 1;
+            else  mosi_pin = test_case_addr[i];
+            sclk_pin = 0;
+            #100 sclk_pin = 1; #100;
+         end
+
+         // Check the output of MISO
+         for (i = 0; i < 10; i = i + 1) begin
+            sclk_pin = 1;
+            if (i > 7) cs_pin = 1;
+            #100 sclk_pin = 0; #100;
+            // test_case_read[i] = miso_pin;
+            if (test_case_read != 8'b1011011) $display("Failed to read 1011011 from register %b", test_case_addr);
+         end
+      end
+      $finish;
+
    end
 endmodule
